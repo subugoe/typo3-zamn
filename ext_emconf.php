0dd23453385f91c46d857f87241a95a70e602d60
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2015-07-20 13:54:45 +0200

    [BUGFIX] Add correct URL for zamn

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,36 +11,32 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'zamn',
-	'description' => 'Zentralarchiv mathematischer Nachlässe',
-	'category' => 'plugin',
-	'author' => 'Marianna Muehlhölzer',
-	'author_email' => 'mmuehlh@sub.uni-goettingen.de',
-	'shy' => '',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'module' => '',
-	'state' => 'alpha',
-	'internal' => '',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 1,
-	'lockType' => '',
-	'author_company' => '',
-	'version' => '1.0.1',
-	'constraints' => array(
-		'depends' => array(
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"f4af";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"c960";s:14:"ext_tables.php";s:4:"0eaa";s:14:"ext_tables.sql";s:4:"936e";s:16:"icon_tx_zamn.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"9732";s:10:"README.txt";s:4:"1e1d";s:7:"tca.php";s:4:"a4e2";s:19:"doc/wizard_form.dat";s:4:"abd1";s:20:"doc/wizard_form.html";s:4:"4b42";s:25:"pi1/class.tx_zamn_pi1.php";s:4:"22e8";s:17:"pi1/locallang.xml";s:4:"c50c";s:24:"pi1/static/editorcfg.txt";s:4:"c581";s:21:"pi1/static/siegel.tif";s:4:"8a85";}',
-	'suggests' => array(
-	),
+    'title' => 'zamn',
+    'description' => 'Zentralarchiv mathematischer Nachlässe',
+    'category' => 'plugin',
+    'author' => 'Marianna Muehlhölzer',
+    'author_email' => 'mmuehlh@sub.uni-goettingen.de',
+    'shy' => '',
+    'dependencies' => '',
+    'conflicts' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'alpha',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 1,
+    'lockType' => '',
+    'author_company' => '',
+    'version' => '1.0.1',
+    'constraints' => array(
+        'depends' => array(),
+        'conflicts' => array(),
+        'suggests' => array(),
+    ),
+    '_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"f4af";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"c960";s:14:"ext_tables.php";s:4:"0eaa";s:14:"ext_tables.sql";s:4:"936e";s:16:"icon_tx_zamn.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"9732";s:10:"README.txt";s:4:"1e1d";s:7:"tca.php";s:4:"a4e2";s:19:"doc/wizard_form.dat";s:4:"abd1";s:20:"doc/wizard_form.html";s:4:"4b42";s:25:"pi1/class.tx_zamn_pi1.php";s:4:"22e8";s:17:"pi1/locallang.xml";s:4:"c50c";s:24:"pi1/static/editorcfg.txt";s:4:"c581";s:21:"pi1/static/siegel.tif";s:4:"8a85";}',
+    'suggests' => array(),
 );
 
 ?>
