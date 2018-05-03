988e21e40049f3a7ce839d5197cfc4e53ac3c2a0
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2018-05-03 09:02:21 +0200

    Fix database schema

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,6 +3,6 @@ CREATE TABLE tx_zamn_domain_model_hans (
   pid INT NOT NULL DEFAULT 0,
   hans_id VARCHAR(20) NOT NULL DEFAULT '',
   title VARCHAR(255) NOT NULL DEFAULT '',
-  content TEXT NOT NULL DEFAULT '',
-  kalliope VARCHAR(255) NOT NULL DEFAULT ''
+  content TEXT NOT NULL,
+  kalliope TEXT NOT NULL
 );
