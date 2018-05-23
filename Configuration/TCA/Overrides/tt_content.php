8e88fa4fd6b6af017a1eb2458d1220c35d0ae3fb
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2018-05-23 11:21:31 +0200

    Remove all TCA modifications from ext_tables.php

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,3 +1,14 @@
 <?php
 
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin([
+    'LLL:EXT:zamn/locallang_db.xml:tt_content.list_type_pi1',
+    'zamn_pi1',
+], 'list_type');
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'Subugoe.zamn',
+    'Hans',
+    'Zamn Hans'
+);
+
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['zamn_pi1'] = 'layout,select_key';
--- ext_tables.php
+++ ext_tables.php
@@ -3,14 +3,3 @@
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin([
-    'LLL:EXT:zamn/locallang_db.xml:tt_content.list_type_pi1',
-    $_EXTKEY.'_pi1',
-], 'list_type');
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'Subugoe.'.$_EXTKEY,
-    'Hans',
-    'Zamn Hans'
-);
